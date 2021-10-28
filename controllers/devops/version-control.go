38d9c5f84ba966afdd559f46dd5f79ef4d4ddf55
slzcc <hotstone@hotstone.io>
2021-10-28 16:59:26 +0800

    algae ingress add

--- controllers/devops/version-control.go
+++ controllers/devops/version-control.go
@@ -5,6 +5,10 @@ import (
 	"log"
 )
 
+func GetHeaderByName(ctx *gin.Context, key string) string {
+	return ctx.Request.Header.Get(key)
+}
+
 func VersionControlEntrance(c *gin.Context) {
 
 	json := make(map[string]interface{})
@@ -14,6 +18,12 @@ func VersionControlEntrance(c *gin.Context) {
 	buf := make([]byte, 1024)
 	n, _ := c.Request.Body.Read(buf)
 	log.Printf("buf %v",string(buf[0:n]))
+
+	event := GetHeaderByName(c, "X-Gitlab-Event")
+	log.Printf("event %v", event)
+
+	token := GetHeaderByName(c, "X-Gitlab-Token")
+	log.Printf("event %v", token)
 
 	c.JSON(200, json)
 }
